14d085494247b5c8210d4e19fa4351efbf4a016b
sOM2H <m1kh41l.melnyk@gmail.com>
2019-08-16 13:52:36 +0300

    Part 4

---
+++ OOP/models/station.rb
@@ -0,0 +1,35 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def add_train train
+    return @trains.push(train) if valid? train
+    raise_not_valid_object
+  end
+
+  def print_trains
+    @trains.each{ |t| puts t.number}
+  end
+
+  def print_trains_by_type
+    p @trains.each_with_object({}) { |t, hsh| hsh[t.type] = (hsh[t.type] || []) << t.number }
+  end
+  
+  def departure_train train_name
+    @trains.delete_if { |t| t.name == train_name }
+  end
+
+  private
+
+  def valid? object
+    object.class.name == "Train"
+  end
+
+  def raise_not_valid_object object
+    raise "#{object} class must equals '#{object}'."
+  end
+end
